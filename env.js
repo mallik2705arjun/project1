c5364753c17034dd876086920092e6dc88f35b19
mallik2705arjun <mallikarjun272004@gmail.com>
2026-04-06 16:36:39 +0530

    Revert "3"
    This reverts commit 06f7b1ad1ee3e2eb9a816cfef960eb94c5e42ebe.

--- env.js
+++ env.js
@@ -4,7 +4,7 @@
 import {z,ZodError} from "zod";
 
 const ageSchema = z.number().min(18).max(100).int();
-const userAge = 40000;
+const userAge = 17;
 
 const parseUserAge =ageSchema.parse(userAge);
 const {data, error, success} = ageSchema.safeParse(userAge);
